da99f4e8529ff5c10ab52c906b4dca82c09fabf3
Calvin Nguyen <calvin.nvqc@gmail.com>
2020-02-21 18:50:36 -0800

    upload working app

---
+++ webapps/app/WEB-INF/classes/HelloServlet.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet("/sayhello")   // Configure the request URL for this servlet (Tomcat 7/Servlet 3.0 upwards)
+public class HelloServlet extends HttpServlet {
+    // The doGet() runs once per HTTP GET request to this servlet.
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+
+        // Set the response MIME type of the response message
+        response.setContentType("text/html");
+        // Allocate a output writer to write the response message into the network socket
+        PrintWriter out = response.getWriter();
+
+        // Write the response message, in an HTML page
+        out.println("<html>");
+        out.println("<head><title>Hello, World</title></head>");
+        out.println("<body>");
+        out.println("<h1>Hello, world!</h1>");  // says Hello
+        // Echo client's request information
+        out.println("<p>Request URI: " + request.getRequestURI() + "</p>");
+        out.println("<p>Protocol: " + request.getProtocol() + "</p>");
+        out.println("<p>PathInfo: " + request.getPathInfo() + "</p>");
+        out.println("<p>Remote Address: " + request.getRemoteAddr() + "</p>");
+        // Generate a random number upon each request
+        out.println("<p>A Random Number: <strong>" + Math.random() + "</strong></p>");
+        out.println("</body></html>");
+        out.close();  // Always close the output writer
+    }
+}
